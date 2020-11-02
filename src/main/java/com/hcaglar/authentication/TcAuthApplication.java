316ca9644df45ac990006cbda1ca7bfee70afcda
hcaglar-com <hcaglar055@gmail.com>
2020-11-02 12:32:59 +0300

    starting point

---
+++ src/main/java/com/hcaglar/authentication/TcAuthApplication.java
@@ -0,0 +1,13 @@
+package com.hcaglar.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcaglar/authentication/TcAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcaglar.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
